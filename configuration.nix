558fc40f42cddbc5a3344fd496773abe1962eb23
saylesss88 <saylesss87@proton.me>
2025-06-16 19:21:24 -0400

    vim git

--- configuration.nix
+++ configuration.nix
@@ -82,10 +82,10 @@
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
