848ba051b21b853858df57249ba79a5837b34b9c
saylesss88 <saylesss87@proton.me>
2025-07-04 20:17:36 -0400

    added functionality
    test 1

--- networking.nix
+++ networking.nix
@@ -1,11 +1,6 @@
 {...}: {
   networking = {
     networkmanager.enable = true;
-    # firewall = {
-    #   enable = true;
-    # allowedTCPPorts = [80 443 25];
-    # allowedUDPPorts = [53 22];
-    # };
     enableIPv6 = true;
   };
 }
